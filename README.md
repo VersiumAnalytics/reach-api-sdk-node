443e43af99321b109c45dbbcb32da3ee263a7bd1
Ryan Watson <rwatson@Ryan-Ws-MacBook-Pro.local>
2022-08-30 15:25:35 -0700

    Added retries, implemented Axios, made code more readable and modularized

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Using Versium REACH with NodeJS   
 1. Clone the repo
-2. Do `npm install node-fetch` in order to have access to the node-fetch library for API calls
+2. Do `npm install axios` in order to have access to the axios library for API calls
 3. Add `"type": "module"` to the nearest parent package.json file
-4. Call the `append` function using `await`
+4. Add  `import Reach from './Versium_REACH.js';`
+5. Create a new instance of the `Reach` class with your API Key
+6. Call the `append` function using the newly created instance of `Reach`
+7. Don't forget to `await` the response from the `append` function
    
 Make sure to check the comments in Versium_REACH.js for more information on the parameters of the `append` function   
    
 ## Things to keep in mind
-- The default rate limit is 20 queries per second   
+- The default rate limit is 20 queries per second
 - You must have a provisioned API key for this function to work. If you are unsure where to find your API key, look at our [API key documentation](https://api-documentation.versium.com/docs/find-your-api-key)   
--- Versium_REACH.js
+++ Versium_REACH.js
@@ -1,120 +1,147 @@
-import fetch from "node-fetch";
-/**
- * This function should be used to effectively query Versium REACH APIs. See our API Documentation for more information
- * https://api-documentation.versium.com/reference/welcome
- *
- * @param  array  $inputData
- * inputData is an array of objects where the keys are the headers and the values are the values corresponding to each header
- * ex. inputData = [{first: "someFirstName", last: "someLastName", email: "someEmailAddress"}];
- * @param  array  outputTypes
- * This array should contain a list of strings where each string is a desired output type. This parameter is optional if the API you are using does not require output types
- * ex. [] or ["email", "phone"]
- * @param  string apiKey
- * @param  string dataTool
- * the current options for dataTool are: contact, demographic, b2cOnlineAudience, b2bOnlineAudience, firmographic, c2b, iptodomain, hemtobusinessdomain
- * @return Promise
- * When the promise resolves, it will be an array of objects
- */
-async function append(inputData, outputTypes, apiKey, dataTool) {
-    let results = new Array();
-    let baseURL = `https://api.versium.com/v2/${dataTool}?`;
-    let urls = Array(inputData.length).fill("");
-    if (outputTypes.length > 0) {
-        outputTypes.forEach(outputType => {
-            baseURL += `output[]=${outputType}&`;
-        });
+import axios from "axios";
+
+class Reach {
+    #apiKey;
+    verbose;
+    waitTime;
+
+    /**
+     * 
+     * @param string apiKey 
+     * @param boolean verbose 
+     * set to "true" to log extra debugging/error information
+     * @param int waitTime 
+     * waitTime is the amount of milliseconds that the program execution will pause for before retrying
+     * requests that had a 429 or 500 response. The program will not pause if there were none of these responses
+     */
+    constructor(apiKey, verbose = false, waitTime = 3000) {
+        this.#apiKey = apiKey;
+        this.verbose = verbose;
+        this.waitTime = waitTime;
     }
-    const options = {
-        headers: {
-            "Accept": "application/json",
-            "x-versium-api-key": apiKey,
+
+    /**
+     * This function should be used to effectively query Versium REACH APIs. See our API Documentation for more information
+     * https://api-documentation.versium.com/reference/welcome
+     *
+     * @param  string dataTool
+     * @param  array  $inputData
+     * inputData is an array of objects where the keys are the headers and the values are the values corresponding to each header
+     * ex. inputData = [{first: "someFirstName", last: "someLastName", email: "someEmailAddress"}];
+     * @param  array  outputTypes
+     * This array should contain a list of strings where each string is a desired output type. This parameter is optional if the API you are using does not require output types
+     * ex. [] or ["email", "phone"]
+     * @return Promise
+     * When the promise resolves, it will be an array of objects
+     */
+    async append(dataTool, inputData, outputTypes = []) {
+        const start = new Date();
+        let baseURL = `https://api.versium.com/v2/${dataTool}?`;
+        let urls = Array(inputData.length).fill("");
+        if (outputTypes.length > 0) {
+            outputTypes.forEach(outputType => {
+                baseURL += `output[]=${outputType}&`;
+            });
         }
-    };
-    if (inputData.length > 0) {
-        inputData.forEach((row, index) => {
-            const inputParams = new URLSearchParams(row).toString();
-            urls[index] = baseURL + inputParams;
-        });
-    } else {
-        throw Error("You have no input data");
-    }
-
-    const batchSize = 3; //max batchSize to avoid running into the rate limit
-    let curReq = 0;
-    while (curReq < urls.length) {
-        const end = urls.length < curReq + batchSize ? urls.length : curReq + batchSize;
-        const concurrentReq = new Array();
-        for (let index = curReq; index < end; index++) {
-            concurrentReq.push(fetch(urls[curReq], options).then((resp) => resp.json()).then((jsonResp) => jsonResp.versium.results));
-
-            console.log(`sending request ${curReq}...`)
-            curReq++;
+        const options = {
+            headers: {
+                "Accept": "application/json",
+                "x-versium-api-key": this.#apiKey,
+            }
+        };
+        if (inputData.length > 0) {
+            inputData.forEach((row, index) => {
+                const inputParams = new URLSearchParams(row).toString();
+                urls[index] = baseURL + inputParams;
+            });
+        } else {
+            throw Error("No input data was entered");
         }
-        const batchResp = await Promise.all(concurrentReq);
-        batchResp.forEach((resp) => {
-            results = [...results, resp];
-        });
-        console.log(`requests ${curReq - batchSize}-${curReq} done.`);
+
+        var retryUrls = { retry: [] };
+        const batchSize = 3; //max batchSize to avoid running into the rate limit
+        let results = await this.#sendHttpRequests(urls, options, batchSize, true, retryUrls)
+        if (retryUrls.retry.length > 0) { //retry urls that failed with 429 or 500 status response on the first attempt
+            await this.#shortSleep(this.waitTime);
+            results = results.concat(await this.#sendHttpRequests(retryUrls.retry, options, batchSize, false));
+        }
+        
+        const end = new Date();
+        if (this.verbose) {
+            console.log(JSON.stringify(results));
+            console.log("Total Results Returned: " + results.length);
+            console.log(`Time Taken to Execute: ${(end - start)/1000} seconds`); 
+        }
+        
+        return results;
     }
 
-    return results;
+    async #sendHttpRequests(urls, options, batchSize, firstAttempt, retryUrls = null) {
+        let curReq = 0;
+        let httpErr429Count = 0;
+        let httpErr500Count = 0;
+        let results = [];
+        while (curReq < urls.length) {
+            const end = urls.length < curReq + batchSize ? urls.length : curReq + batchSize;
+            const concurrentReq = new Array();
+            for (let index = curReq; index < end; index++) {
+                concurrentReq.push(axios.get(urls[curReq], options)
+                .then((response) => response.data.versium.results)
+                .catch((err) => {
+                    if (firstAttempt && (err.response.status == 429 || err.response.status == 500)) {
+                        retryUrls.retry.push(urls[curReq]);
+                        if (err.response.status == 429) {
+                            httpErr429Count++;
+                        } else if (err.response.status == 500) {
+                            httpErr500Count++;
+                        }
+                    }
+                    if (this.verbose) {
+                        console.log(err);
+                    }
+                }));
+                if (this.verbose) {
+                    if (firstAttempt) {
+                        console.log(`sending request ${curReq}...`);
+                    } else {
+                        console.log(`sending retry request ${curReq}...`);
+                    }
+                }
+                curReq++;
+            }
+            const batchResp = await Promise.all(concurrentReq);
+            batchResp.forEach((resp) => {
+                results = [...results, resp];
+            });       
+            if (this.verbose) {
+                if (curReq == urls.length && (curReq % batchSize != 0)) { //happens at the end of the last batch
+                    console.log(`requests ${curReq - batchSize + 1}-${curReq - 1} done.`);
+                } else {
+                    console.log(`requests ${curReq - batchSize}-${curReq - 1} done.`);
+                }
+            }
+        }
+        results = results.filter((result) => result != null);
+
+        if (firstAttempt && this.verbose) {
+            console.log("Successful Requests Made: " + results.length);
+            if (httpErr429Count > 0) {
+                console.log("There were " + httpErr429Count + " requests with a 429 http status code");
+            } else if (httpErr500Count > 0) {
+                console.log("There were " + httpErr500Count + " requests with a 500 http status code");
+            }
+        } else if (!firstAttempt && this.verbose) {
+            console.log(`There were ${urls.length ? urls.length : 0} requests that were retried`);
+            console.log(`There were ${results.length} requests that retried successfully`);
+        }
+
+        return results;
+    }
+
+    //sleeps the function execution temporarily
+    #shortSleep(milliseconds) {
+        return new Promise(resolve => setTimeout(resolve, milliseconds)); 
+    }
 }
 
-//USE THIS METHOD FOR TESTING, DELETE IT BEFORE PUBLISHING THE REPO
-//uncomment/change the code block according to the api you want to use, then run the program
-async function testMethod() {
-    //c2b test data
-    // const testInputData = Array(135).fill({ first: "Angela", last: "Adams", email: "adamsangela@hotmail.com" });
-    // const testOutputTypes = [];
-    // const testApiKey = '';
-    // const testDataTool = 'c2b';
-
-    //iptodomain test data
-    // const testInputData = Array(97).fill({ ip: "152.44.212.248" });
-    // const testOutputTypes = [];
-    // const testApiKey = '';
-    // const testDataTool = 'iptodomain';
-
-    //contact append test data
-    // const testInputData = Array(101).fill({ email: "adamsangela@hotmail.com" });
-    // const testOutputTypes = ["address", "phone"];
-    // const testApiKey = '';
-    // const testDataTool = 'contact';
-
-    //demographic test data
-    // const testInputData = Array(88).fill({ email: "adamsangela@hotmail.com" });
-    // const testOutputTypes = ["lifestyle", "political"];
-    // const testApiKey = '';
-    // const testDataTool = 'demographic';
-
-    //b2cOnlineAudience test data
-    // const testInputData = Array(95).fill({ first: "Marc", last: "Blythe", email: "marc@blytheglobal.com" });
-    // const testOutputTypes = [];
-    // const testApiKey = '';
-    // const testDataTool = 'b2cOnlineAudience';
-
-    //b2bOnlineAudience test data
-    // const testInputData = Array(95).fill({ first: "Marc", last: "Blythe", email: "marc@blytheglobal.com" });
-    // const testOutputTypes = [];
-    // const testApiKey = '';
-    // const testDataTool = 'b2bOnlineAudience';
-
-    //firmographic test data
-    // const testInputData = Array(87).fill({ email: "marc@blytheglobal.com" });
-    // const testOutputTypes = [];
-    // const testApiKey = '';
-    // const testDataTool = 'firmographic';
-
-    //hemtobusinessdomain test data
-    // const testInputData = Array(5).fill({ email: "a3bf653e8393decc6cde18e15c4469fc" });
-    // const testOutputTypes = [];
-    // const testApiKey = '';
-    // const testDataTool = 'hemtobusinessdomain';
-
-    const responses = await append(testInputData, testOutputTypes, testApiKey, testDataTool);
-    responses.forEach((resp, index) => {
-        console.log('---------RESPONSE ' + index + ' STARTS HERE---------')
-        console.log(resp);
-    });
-}
-testMethod();
+export default Reach;
